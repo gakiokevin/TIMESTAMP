7cb3f72611c1cef66ea1b90be26adf9e4995b03a
gakiokevin <gakiokevin5@gmail.com>
2024-02-28 14:47:40 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,92 @@
+const express = require('express')
+const cors = require('cors')
+
+const moment =  require('moment-timezone')
+const app = express();
+const corsOptions = {
+  origin: '*',
+  methods: 'GET',
+};
+
+app.use(cors(corsOptions))
+
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET');
+  next();
+});
+const PORT = process.env.PORT || 5000
+
+
+app.get('/:home?',(req,res)=>{
+  res.json('hello world')
+})
+
+app.listen(PORT,()=>console.log(`started on port ${PORT}`))
+
+
+
+// app.get('/:date?',(req,res)=>{
+
+
+//   try {
+//     const dateInput = req.params.date || req.query.date;
+//     const results = unixAndUtc(dateInput);
+//     res.json(results);
+//   } catch (error) {
+//     res.status(400).json({ error: error.message }); // Return 400 Bad Request with error message
+//   }
+  
+// })
+
+
+
+// function unixAndUtc(input){
+//   let unix
+//   let utc
+
+//   if(!input){
+//     return getCurrentTime()
+//   }
+//   const dateRegex = /^\d{4}-\d{2}-\d{2}$/
+
+//   if (dateRegex.test(input)){
+//     input +="T00:00:00"
+//   }
+//   const parsedDate = moment(input).utc()
+
+//   if (parsedDate.isValid()) {
+//     unix = parsedDate.valueOf();
+//     utc = parsedDate.toDate();
+//   }
+//   else {
+//     return { error : "Invalid Date" }
+//   }
+// return {
+//   unix:unix,
+//   utc: utc.toUTCString()
+// }
+
+// }
+
+
+// function getCurrentTime(){
+
+//     const currentTimestamp = Date.now();
+//     const currentUtcTime = new Date(currentTimestamp).toUTCString();
+
+//   return {
+//       unix: currentTimestamp,
+//       utc: currentUtcTime
+//   };
+
+
+
+// }
+
+
+
+
+
+// module.exports =app;
